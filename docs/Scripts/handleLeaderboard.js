00cdf885550ddd5c09f9807e117a137868a12914
Cheese Curd <51841108+Cheese-Curd@users.noreply.github.com>
2026-02-05 02:04:28 -0500

    Fetch data for leaderboard

--- docs/Scripts/handleLeaderboard.js
+++ docs/Scripts/handleLeaderboard.js
@@ -1,41 +1,133 @@
 function print(str)
 {
-	console.log("[Leadboard Handler] " + str)
+	console.log("[Leadboard Handler/LOG] " + str)
 }
 
-function getGameData()
-{
-	print("Getting Game Data...")
-	if (catagory == null)
-		catagory = "any%"
+const githubPath = "https://api.github.com/repos/Cheese-Curd/leaderboards/contents/leaderboards/"
 
-	print("Game: " + game)
-	print("Game Name: ")
-	print("Catagory: " + catagory)
+const leaderboardDiv = document.getElementById("leaderboard")
+const loadingTxt     = document.getElementById("loadingTxt")
+const notFoundDiv    = document.getElementById("notFound")
+const titleText      = document.getElementById("headerTitle")
+
+const gameName     = document.getElementById("game")
+const bannerDiv    = document.getElementById("bannerImage")
+const catagoryName = document.getElementById("catagory")
+
+const leaderboardTable = document.querySelector(".lbList");
+
+async function fetchGitHubJSON(url) {
+	const res = await fetch(url);
+	const data = await res.json();
+
+	if (!data.content)
+		throw new Error("[Leaderboard Handler/GITHUB] Not a valid JSON file: " + url);
+
+	const decoded = atob(data.content);
+	return JSON.parse(decoded);
 }
 
-function handleLeaderboard(visible)
+async function fetchGameCatagories(url)
 {
-	const leaderboardDiv = document.getElementById("leaderboard")
+	const res   = await fetch(url)
+	const files = await res.json()
+
+	var catagories = {}
+
+	for (const file of files)
+	{
+		if (file.type !== "file") continue // not a file
+
+		const json = await fetchGitHubJSON(file.url)
+		catagories[file.name.replace(".json", "")] = json;
+	}
+
+	return catagories
+}
+
+async function handleLeaderboard(visible)
+{
+	leaderboardDiv.style.display = "none"
 
 	print("Game Selected: " + visible)
 	if (visible)
 	{
+		const gamePath = githubPath + game
+		loadingTxt.style.display = "block"
 		
+		if ((await fetch(gamePath)).ok)
+		{
+			print("Found game!")
+			const data = await fetchGitHubJSON(gamePath + "/data.json")
+			
+			// This is for the games banner 'n stuff,
+			// basically I want to keep it in the site's root when it's the generic one,
+			// but if it's not that, then fetch it in the game's root
+			var bannerPath = ""
+			if (data.banner == "generic_banner.png")
+				bannerPath = data.banner
+			else
+				bannerPath = `leaderboards/${game}/${data.banner}`
 
-		const gameName = document.getElementById("game")
-		const catagoryName = document.getElementById("catagory")
+			bannerDiv.style.backgroundImage = `url('${bannerPath}')`
+			gameName.innerText = data.name
+			print("Loaded name & banner")
 
-		gameName.innerText = game
-		catagoryName.innerText = catagory
-	}
-	else
-	{
-		leaderboardDiv.style.display = "none"
+			
+			var catagories = await fetchGameCatagories(gamePath + "/catagories")
+			
+			if (catagory == null)
+				catagory = "any%"
+
+			const curCatagory = catagories[catagory]
+
+			// Actual leaderboard stuff
+			catagoryName.innerText = curCatagory.name
+
+			// Handle leaderboard sorting and adding to the table
+			const lboard = curCatagory.leaderboard
+			Object.keys(lboard)
+				.sort((a, b) => Number(a) - Number(b))
+				.forEach(rank => {
+					const entry = lboard[rank];
+					const [runner, RTA, reTimed, date, verified] = entry;
+
+					const tr = document.createElement("tr");
+
+					tr.innerHTML = `
+					<td class="rank">${rank}</td>
+						<td class="runner">${runner}</td>
+						<td>${RTA}</td>
+						<td>${reTimed}</td>
+						<td>${date}</td>
+					<td>${verified ? "Yes" : "No"}</td>
+					`;
+
+					leaderboardTable.appendChild(tr);
+				});
+			
+			// Hide loading text, and show the leaderboard which (hopefully) should have times
+			loadingTxt.style.display = "none"
+			leaderboardDiv.style.display = "block"
+		}
+		else
+		{
+			print("Could not find game!")
+			titleText.innerText = "! 404 !"
+			loadingTxt.style.display = "none"
+			notFoundDiv.style.display = "block"
+		}
 	}
 }
 
-console.log("-[ Leaderboard Handler Loaded ]-")
+console.log(
+	"%c-[ Leaderboard Handler Loaded ]-",
+	"font-size: 24px; font-weight: bold; text-align: center; width: 100%; display: block;"
+);
+console.log(
+	"%c> Version 1.0.0 <",
+	"font-size: 18px; text-align: center; width: 100%; display: block;"
+);
 
 const params   = new URLSearchParams(window.location.search);
 var game     = params.get("game")
